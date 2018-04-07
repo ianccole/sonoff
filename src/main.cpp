aab17d9a3cfbdd365d5d9a69cc0d794c9b25720d
ian cole <ianccole@gmail.com>
2018-04-07 10:46:30 +0100

    add ds18b20 to sonoff GPIO 14

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,26 @@
 */
 
 #include <Homie.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+const int PIN_ONE_WIRE = 14;
 const int PIN_RELAY = 12;
 const int PIN_LED = 13;
 const int PIN_BUTTON = 0;
+
+const int TEMPERATURE_INTERVAL = 60;			// seconds
+unsigned long lastTemperatureSent = 0;
+
+OneWire oneWire(PIN_ONE_WIRE);
+DallasTemperature DS18B20(&oneWire);
 
 unsigned long buttonDownTime = 0;
 byte lastButtonState = 1;
 byte buttonPressHandled = 0;
 
 HomieNode switchNode("switch", "switch");
+HomieNode temperatureNode("temperature", "temperature");
 
 bool switchOnHandler(HomieRange range, String value) {
   if (value != "true" && value != "false") return false;
@@ -36,6 +46,17 @@ void toggleRelay() {
 }
 
 void loopHandler() {
+    if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
+      float temperature = 22; // Fake temperature here, for the example
+
+      DS18B20.requestTemperatures();
+      temperature = DS18B20.getTempCByIndex(0);
+
+      Homie.getLogger() << "Temperature: " << temperature << " °C" << endl;
+      temperatureNode.setProperty("degrees").send(String(temperature));
+      lastTemperatureSent = millis();
+    }
+  
   byte buttonState = digitalRead(PIN_BUTTON);
   if ( buttonState != lastButtonState ) {
     if (buttonState == LOW) {
@@ -53,6 +74,11 @@ void loopHandler() {
   }
 }
 
+void setupHandler()
+{
+	temperatureNode.setProperty("unit").send("c");
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
@@ -61,11 +87,12 @@ void setup() {
   pinMode(PIN_BUTTON, INPUT);
   digitalWrite(PIN_RELAY, LOW);
 
-  Homie_setFirmware("itead-sonoff-buton", "1.0.2");
+  Homie_setFirmware("itead-sonoff-buton", "1.0.3");
   Homie.setLedPin(PIN_LED, LOW).setResetTrigger(PIN_BUTTON, LOW, 5000);
 
   switchNode.advertise("on").settable(switchOnHandler);
 
+ 	Homie.setSetupFunction(setupHandler);
   Homie.setLoopFunction(loopHandler);
   Homie.setup();
 }
